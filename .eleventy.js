d3a827d382ad8d517f34392808c215f3462851f3
Peter deHaan <peter@deseloper.com>
2022-01-26 15:03:01 -0800

    Better .eleventy.js

--- .eleventy.js
+++ .eleventy.js
@@ -1,22 +1,11 @@
 const fs = require("fs");
 
 module.exports = (eleventyConfig) => {
-eleventyConfig.addShortcode("photoFolder", function (photoFolder) {
-  const html = fs.readdirSync(photoFolder)
-    .map((file) => `<img src="${file}" />`);
-  return html.join("\n");
-});
-
-// eleventyConfig.addShortcode("photoFolder", function (photoFolder) {
-//   const photoList = [];
-//   fs.readdir(photoFolder, (err, files) => {
-//     if (err) throw err;
-//     files.forEach((file) => {
-//       photoList.push(`<img src="${photoFolder}/${file}" />`);
-//     });
-//   });
-//   return photoList.join("\n");
-// });
+  eleventyConfig.addShortcode("photoFolder", function (photoFolder) {
+    const html = fs.readdirSync(photoFolder)
+      .map((file) => `<img src="${file}" />`);
+    return html.join("\n");
+  });
 
   eleventyConfig.addPassthroughCopy("src/assets");
 
